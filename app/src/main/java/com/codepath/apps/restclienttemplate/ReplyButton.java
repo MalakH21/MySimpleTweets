03fe41776ef1141df6ca6f918b83e52176f239db
Malak Hmimy <malakhmimy@fb.com>
2018-07-06 18:20:49 -0700

    tweet can finally be replied to

--- app/src/main/java/com/codepath/apps/restclienttemplate/ReplyButton.java
+++ app/src/main/java/com/codepath/apps/restclienttemplate/ReplyButton.java
@@ -1,14 +1,17 @@
 package com.codepath.apps.restclienttemplate;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
 import com.codepath.apps.restclienttemplate.models.Tweet;
 import com.loopj.android.http.JsonHttpResponseHandler;
 
+import org.json.JSONException;
 import org.json.JSONObject;
 import org.parceler.Parcels;
 
@@ -35,8 +38,8 @@ public class ReplyButton extends AppCompatActivity {
         simpleEditText = (EditText) findViewById(R.id.et_simple);
         String strValue = simpleEditText.getText().toString();
         client = TwitterApp.getRestClient(this);
-
-        EditText et_simple = findViewById(R.id.et_simple);
+        setText();
+        //EditText et_simple = findViewById(R.id.et_simple);
 
         tweet = (Tweet) Parcels.unwrap(getIntent().getParcelableExtra(Tweet.class.getSimpleName()));
 
@@ -46,32 +49,83 @@ public class ReplyButton extends AppCompatActivity {
 //        et_simple.setText(tweet.user.screenName);
 
         button = findViewById(R.id.button);
+    }
 
+    public void onClick(View view) {
 
-        button.setOnClickListener(new View.OnClickListener() {
+        client.ReplyButton(simpleEditText.getText().toString(), new JsonHttpResponseHandler() {
             @Override
-            public void onClick(View v) {
-                client.ReplyButton(simpleEditText.getText().toString(), new JsonHttpResponseHandler(){
+            public void onSuccess(int statusCode, Header[] headers, JSONObject response) {
+                try {
+                    Tweet tweet = Tweet.fromJSON(response);
+                    Intent data = new Intent();
+                    data.putExtra("tweet", Parcels.wrap(tweet));
+                    setResult(RESULT_OK, data);
+                    finish();
 
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
 
-                    @Override
-                    public void onFailure(int statusCode, Header[] headers, String responseString, Throwable throwable) {
-                        super.onFailure(statusCode, headers, responseString, throwable);
-                    }
-
-                    @Override
-                    public void onSuccess(int statusCode, Header[] headers, JSONObject response) {
-                        super.onSuccess(statusCode, headers, response);
-                    }
-
-
-                });
+            }
+            @Override
+            public void onFailure(int statusCode, Header[] headers, Throwable throwable, JSONObject errorResponse) {
+                Toast.makeText(getApplicationContext(), "something wrong", Toast.LENGTH_LONG).show();
             }
         });
-
-
     }
-}
+
+
+//            public void onClick(View v) {
+//                client.ReplyButton(simpleEditText.getText().toString(), new JsonHttpResponseHandler() {
+//                    @Override
+//                    public void onSuccess(int statusCode, Header[] headers, JSONObject response) {
+//                        try {
+//                            Tweet tweet = Tweet.fromJSON(response);
+//                            Intent data = new Intent();
+//                            data.putExtra("tweet", Parcels.wrap(tweet));
+//                            setResult(RESULT_OK, data);
+//                            finish();
+//
+//                        } catch (JSONException e) {
+//                            e.printStackTrace();
+//                        }
+//
+//                    }
+//
+//                    @Override
+//                    public void onFailure(int statusCode, Header[] headers, Throwable throwable, JSONObject errorResponse) {
+//                        Toast.makeText(getApplicationContext(), "something wrong", Toast.LENGTH_LONG).show();
+//                    }
+//                });
+//            }
+//        }
+
+
+
+//        button.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                client.ReplyButton(simpleEditText.getText().toString(), new JsonHttpResponseHandler(){
+//
+//
+//                    @Override
+//                    public void onFailure(int statusCode, Header[] headers, String responseString, Throwable throwable) {
+//                        super.onFailure(statusCode, headers, responseString, throwable);
+//                    }
+//
+//                    @Override
+//                    public void onSuccess(int statusCode, Header[] headers, JSONObject response) {
+//                        super.onSuccess(statusCode, headers, response);
+//                    }
+//
+//
+//                });
+//            }
+//        });
+    }
+
+
 
 
 
